e109a7b4b692981923dde325fcd4fd2c79f50021
arunMuruganDev <121711565+arunMuruganDev@users.noreply.github.com>
2023-03-10 23:01:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function signInForm()
+{
+    document.querySelector(".signin").style.display = "block";
+    document.querySelector(".signup").style.display = "none";
+}
+function signUpForm()
+{
+    document.querySelector(".signin").style.display = "none";
+    document.querySelector(".signup").style.display = "block";
+}
